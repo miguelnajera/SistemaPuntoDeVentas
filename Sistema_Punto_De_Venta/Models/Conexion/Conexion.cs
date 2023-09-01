ec99e99a364dc9f6b82ed6301cfaa45b82218d0e
Miguel <angelnajera21@hotmail.com>
2023-08-31 23:14:37 -0600

    Ultima modificacion al 31/08/2023 GetTable aun no funciona.

---
+++ Sistema_Punto_De_Venta/Models/Conexion/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using LinqToDB;
+using LinqToDB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Conexion
+{
+    public class Conexion : DataConnection
+    {
+        public ITable<TClientes> TClientes { get { return GetTable<TClientes>(); } }
+        
+    }
+}
